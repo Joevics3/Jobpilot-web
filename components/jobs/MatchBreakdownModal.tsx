e25b041b09d1cce9a5478bf78192aeb008cbdf18
Joevics <joevicsvictor@gmail.com>
2026-01-03 16:12:10 +0100

    4nd batch fixes- color changing

--- components/jobs/MatchBreakdownModal.tsx
+++ components/jobs/MatchBreakdownModal.tsx
@@ -9,7 +9,6 @@ import {
 } from '@/components/ui/dialog';
 import { X } from 'lucide-react';
 import { theme } from '@/lib/theme';
-import { theme } from '@/lib/theme';
 
 export interface MatchBreakdown {
   rolesScore: number;
