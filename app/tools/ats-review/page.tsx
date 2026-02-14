92a95a0a6e7811748c1d7da6f00678c7ebf17623
Joevics <joevicsvictor@gmail.com>
2026-02-14 19:18:56 +0100

    fixing tools

--- app/tools/ats-review/page.tsx
+++ app/tools/ats-review/page.tsx
@@ -41,14 +41,6 @@ export default function ATSReviewPage() {
     return theme.colors.match.bad;
   };
 
-  if (loading) {
-    return (
-      <div className="min-h-screen flex items-center justify-center bg-gray-50">
-        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-green-600"></div>
-      </div>
-    );
-  }
-
   return (
     <div className="min-h-screen bg-gray-50">
       {/* Header */}
